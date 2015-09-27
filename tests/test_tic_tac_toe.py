9befa3e8dfed14acef2d90c9fb2a63e8217ca702
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2015-09-27 18:15:29 +0100

    Better TicTacToe tests
    Increase depth to 10, remove center move and add abother blocking scenario

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -3,31 +3,31 @@ from examples.tic_tac_toe import TicTacToeState, TicTacToeMove
 from gtsa.gtsa import Minimax
 
 
-def test_tic_tac_toe_center():
-    state = TicTacToeState(3, "___"
-                              "___"
-                              "___")
-    algorithm = Minimax('X', 'O', 1)
-    assert algorithm.get_move(state) == TicTacToeMove(1, 1)
-
 def test_tic_tac_toe_finish():
     state = TicTacToeState(3, "XX_"
                               "___"
                               "___")
-    algorithm = Minimax('X', 'O', 1)
+    algorithm = Minimax('X', 'O', 10)
     assert algorithm.get_move(state) == TicTacToeMove(2, 0)
 
 def test_tic_tac_toe_block():
     state = TicTacToeState(3, "O__"
                               "O__"
                               "___")
-    algorithm = Minimax('X', 'O', 1)
+    algorithm = Minimax('X', 'O', 10)
     assert algorithm.get_move(state) == TicTacToeMove(0, 2)
+
+def test_tic_tac_toe_block_2():
+    state = TicTacToeState(3, "X__"
+                              "OO_"
+                              "___")
+    algorithm = Minimax('X', 'O', 10)
+    assert algorithm.get_move(state) == TicTacToeMove(2, 1)
 
 def test_tic_tac_toe_terminal():
     state = TicTacToeState(3, "XOX"
                               "OOX"
                               "OXO")
-    algorithm = Minimax('X', 'O', 1)
+    algorithm = Minimax('X', 'O', 10)
     with pytest.raises(ValueError):
         algorithm.get_move(state)
