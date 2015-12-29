2dbd967ed9f8d7300895459e5154e5792f06b3e3
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2015-12-29 20:36:30 +0000

    Fix PEP 8

--- python/gtsa.py
+++ python/gtsa.py
@@ -50,18 +50,25 @@ def cmp_to_key(mycmp):
     class Key:
         def __init__(self, obj, *args):
             self.obj = obj
+
         def __lt__(self, other):
             return mycmp(self.obj, other.obj) < 0
+
         def __gt__(self, other):
             return mycmp(self.obj, other.obj) > 0
+
         def __eq__(self, other):
             return mycmp(self.obj, other.obj) == 0
+
         def __le__(self, other):
             return mycmp(self.obj, other.obj) <= 0
+
         def __ge__(self, other):
             return mycmp(self.obj, other.obj) >= 0
+
         def __ne__(self, other):
             return mycmp(self.obj, other.obj) != 0
+
         def __hash__(self):
             return hash(self.obj)
     return Key
